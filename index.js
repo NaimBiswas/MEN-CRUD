9d98eef330e969cfd3790acf0ee2f07eb4a6e3f9
NaimBiswas <nayeembiswas2@gmail.com>
2021-04-18 21:15:19 +0600

    Add mongoose server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require("mongoose")
 const app = express()
 const port = 3000
 
@@ -6,3 +7,22 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
+
+
+
+
+
+
+// Connect MongoDB at default port 27017.
+mongoose.connect('mongodb://localhost:27017/Alien', {
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useUnifiedTopology: true,
+}, (err) => {
+   if (!err) {
+      console.log('MongoDB Connection Succeeded.')
+   } else {
+      console.log('Error in DB connection: ' + err)
+   }
+});
+
